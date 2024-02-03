0d6bdbc4bd319fe0cbc7c38f90dabf5ca3d49013
Yannick <yannick.kupferschmidt@googlemail.com>
2024-02-03 11:17:40 +0100

    fix: logo app

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -32,7 +32,7 @@
                             <!-- Logo -->
                             <div class="shrink-0 flex items-center">
                                 <a href="/dashboard">
-                                    <img src="{{ asset('images/logo_trainercontainer.png') }}" class="block h-9 w-auto" style="height: 10rem;" />
+                                    <img src="{{ asset('images/logo_trainercontainer.png') }}" class="block h-12 w-auto my-5" />
                                 </a>
                             </div>
 

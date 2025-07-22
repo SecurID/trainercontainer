7750cc2066c7530e5ad2bbf592b74f03c7998d6b
SecurID <yannick.kupferschmidt@googlemail.com>
2025-07-22 18:13:44 +0200

    feat: make player list searchable

---
+++ app/Livewire/PlayersList.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace App\Livewire;
+
+use Livewire\Component;
+use Illuminate\Support\Facades\Auth;
+
+class PlayersList extends Component
+{
+    public string $search = '';
+
+    public function render()
+    {
+        $query = Auth::user()->players();
+        
+        if (!empty($this->search)) {
+            $query->where(function($q) {
+                $q->where('prename', 'LIKE', '%' . $this->search . '%')
+                  ->orWhere('lastname', 'LIKE', '%' . $this->search . '%');
+            });
+        }
+        
+        $players = $query->orderBy('players.lastname')->get();
+
+        return view('livewire.players-list', compact('players'));
+    }
+}
---
+++ resources/views/livewire/players-list.blade.php
@@ -0,0 +1,25 @@
+<div>
+    <div class="mb-4">
+        <input 
+            type="text" 
+            wire:model.live="search" 
+            placeholder="{{ __('Search players...') }}" 
+            class="w-full px-4 py-2 border border-gray-300 rounded-lg focus:ring-2 focus:ring-primary-500 focus:border-primary-500"
+        />
+    </div>
+
+    <div>
+        @if(count($players) === 0)
+            @if(empty($search))
+                <p class="p-2">{{ __('No players found.') }} {{ __('Create one by clicking on "Create Player".') }}</p>
+            @else
+                <p class="p-2">{{ __('No players found matching your search.') }}</p>
+            @endif
+        @endif
+        @foreach($players as $player)
+            <a href="{{ route('players.show', $player->id) }}" class="block px-4 py-2 mt-2 bg-gray-100 rounded hover:bg-gray-200">
+                {{$player->lastname}}, {{$player->prename}}
+            </a>
+        @endforeach
+    </div>
+</div>

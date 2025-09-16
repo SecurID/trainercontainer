129f57c71da31683ffa14fb1e9db4628b87d8324
SecurID <yannick.kupferschmidt@googlemail.com>
2025-09-16 22:08:39 +0200

    chore: update composer

--- resources/lang/de/validation.php
+++ resources/lang/de/validation.php
@@ -38,6 +38,7 @@ return [
     'digits_between'         => ':Attribute muss zwischen :min und :max Stellen haben.',
     'dimensions'             => ':Attribute hat ungültige Bildabmessungen.',
     'distinct'               => ':Attribute beinhaltet einen bereits vorhandenen Wert.',
+    'doesnt_contain'         => ':Attribute darf keines der folgenden enthalten: :values.',
     'doesnt_end_with'        => ':Attribute darf nicht mit einem der folgenden enden: :values.',
     'doesnt_start_with'      => ':Attribute darf nicht mit einem der folgenden beginnen: :values.',
     'email'                  => ':Attribute muss eine gültige E-Mail-Adresse sein.',
